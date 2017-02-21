d1b6a375c94375e55edbc5f915affc4dc9d0b2cd
bethannfrankum <bethannfrankum88@gmail.com>
2017-02-21 11:03:52 -0600

    attempting to apply some sass to my special offer button

--- js/side-navjquery.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $('.dLabel').on('click', function (e) {
-        e.preventDefault();
-        var btn = $(this);
-        var nav = btn.closest('.dropdown');
-        nav.find('.dropdown-menu').slideToggle();
-    });
-});
